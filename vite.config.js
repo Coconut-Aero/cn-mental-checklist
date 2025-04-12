657a05c2736bb9cd024601acaeb4b91fffde9b25
Coconut-Aero <148566655+Coconut-Aero@users.noreply.github.com>
2025-04-12 15:12:38 +0800

    [FIX] fixed the refresh problem on Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   base: '/cn-mental-checklist/',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '',   // 复制到 dist 根目录
+          rename: '404.html'
+        }
+      ]
+    })
+  ],
 })
